9d43d60a8f3dcac3c331b1e94943d64adf704930
Maria Camenzuli <maria.camenzuli93@gmail.com>
2019-06-02 15:52:53 +0100

    Replacing unittest with assertpy

--- test_wargroove.py
+++ test_wargroove.py
@@ -1,22 +1,16 @@
 import cmp504
 import numpy as np
-import unittest
+from assertpy import assert_that
 
 
-class TestWargroove(unittest.TestCase):
+def test_cherrystone_stronghold_exists():
+    static_templates = {
+        'cherrystone_dragon': 'data/cherrystone_dragon.png',
+        'cherrystone_stronghold': 'data/cherrystone_stronghold.png'
+    }
+    displayCtrl = cmp504.display_controller.DisplayController(static_templates)
+    mouseCtrl = cmp504.mouse_controller.MouseController()
+    screenshot = displayCtrl.read_image('data/test-screenshot.png')
+    match = displayCtrl.find_template('cherrystone_stronghold', image=screenshot)
 
-    def test_cherrystone_stronghold_exists(self):
-        static_templates = {
-            'cherrystone_dragon': 'data/cherrystone_dragon.png',
-            'cherrystone_stronghold': 'data/cherrystone_stronghold.png'
-        }
-        displayCtrl = cmp504.display_controller.DisplayController(static_templates)
-        mouseCtrl = cmp504.mouse_controller.MouseController()
-        screenshot = displayCtrl.read_image('data/test-screenshot.png')
-        match = displayCtrl.find_template('cherrystone_stronghold', image=screenshot)
-
-        self.assertEqual(np.shape(match)[1], 1, "Cherrystone stronghold not located")
-
-
-if __name__ == '__main__':
-    unittest.main()
+    assert_that(np.shape(match)[1]).is_equal_to(1)
