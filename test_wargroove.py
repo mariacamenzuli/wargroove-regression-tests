365647fc78e287cfb3d79417367204b28494ff0c
Maria Camenzuli <maria.camenzuli93@gmail.com>
2019-06-24 15:56:39 +0100

    Finding wargroove icon on taskbar, clicking, then locating building

--- test_wargroove.py
+++ test_wargroove.py
@@ -1,10 +1,21 @@
 import cmp504
+import time
 from assertpy import assert_that
 
 
 def test_cherrystone_stronghold_exists():
     computer_vision_ctrl = cmp504.computer_vision.CVController()
-    computer_vision_ctrl.load_frame("data/test-screenshot.png")
-    match = computer_vision_ctrl.find_template_match("data/cherrystone_stronghold.png")
+    computer_vision_ctrl.capture_frame()
 
-    assert_that(match).is_true()
+    match = computer_vision_ctrl.find_template_match("data/wargroove-icon.PNG")
+
+    mouse_ctrl = cmp504.mouse_controller.MouseController()
+    mouse_ctrl.move_mouse(match[0], match[1])
+    mouse_ctrl.left_mouse_click()
+
+    time.sleep(0.5)
+    computer_vision_ctrl.capture_frame()
+
+    match2 = computer_vision_ctrl.find_template_match("data/cherrystone_stronghold.png")
+
+    assert_that(match2).is_not_none()
