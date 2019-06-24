5d08e33bcf44c8be8dcdbb17184120f742a64d55
Maria Camenzuli <maria.camenzuli93@gmail.com>
2019-06-24 23:11:00 +0100

    Locating some units

--- test_wargroove.py
+++ test_wargroove.py
@@ -15,7 +15,34 @@ def test_cherrystone_stronghold_exists():
 
     time.sleep(0.5)
     computer_vision_ctrl.capture_frame()
+    mouse_over_building(computer_vision_ctrl, mouse_ctrl, "cherrystone_stronghold")
 
-    match2 = computer_vision_ctrl.find_template_match("data/cherrystone_stronghold.png")
+    time.sleep(0.5)
+    computer_vision_ctrl.capture_frame()
+    mouse_over_unit(computer_vision_ctrl, mouse_ctrl, "cherrystone_commander_mercia")
 
-    assert_that(match2).is_not_none()
+    time.sleep(0.5)
+    computer_vision_ctrl.capture_frame()
+    mouse_over_unit(computer_vision_ctrl, mouse_ctrl, "cherrystone_soldier")
+
+    time.sleep(0.5)
+    computer_vision_ctrl.capture_frame()
+    mouse_over_unit(computer_vision_ctrl, mouse_ctrl, "cherrystone_villager")
+
+
+def mouse_over_unit(computer_vision_ctrl, mouse_ctrl, unit_template_name):
+    match = computer_vision_ctrl.find_template_match("data/units/" + unit_template_name + ".png",
+                                                     threshold=0.7,
+                                                     method=cmp504.computer_vision.TemplateMatchingMethod.CROSS_CORRELATION_NORMALIZED)
+
+    assert_that(match).is_not_none()
+    mouse_ctrl.move_mouse(match[0], match[1])
+
+
+def mouse_over_building(computer_vision_ctrl, mouse_ctrl, building_template_name):
+    match = computer_vision_ctrl.find_template_match("data/buildings/" + building_template_name + ".png",
+                                                     threshold=0.7,
+                                                     method=cmp504.computer_vision.TemplateMatchingMethod.CROSS_CORRELATION_NORMALIZED)
+
+    assert_that(match).is_not_none()
+    mouse_ctrl.move_mouse(match[0], match[1])
